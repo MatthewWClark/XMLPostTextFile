4767b85c67b34e373aa33176a78726ee948d7225
MatthewWClark <84600787+MatthewWClark@users.noreply.github.com>
2021-07-27 10:07:17 -0500

    Create XMLCurlPost.sh

---
+++ XMLCurlPost.sh
@@ -0,0 +1,11 @@
+##cats text file into variable
+file1=$(cat /path/example.txt)
+
+##puts contents of file1 into message peramater
+curl -k --location --request POST '(PostURL)' \
+--header 'X-API-Key: (API Key)' \
+--header 'Content-Type: application/xml' \
+--data-raw '<?xml version="1.0" encoding="UTF-8"?>
+  (rest of API content here)
+    <message type="text/plain">'"$file3"'</message>
+</ticket>'
