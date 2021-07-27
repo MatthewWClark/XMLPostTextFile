32fe407fe1281e6f0639497b7045a1de45407e08
MatthewWClark <84600787+MatthewWClark@users.noreply.github.com>
2021-07-27 10:09:00 -0500

    Update XMLCurlPost.sh
    fixed typo

--- XMLCurlPost.sh
+++ XMLCurlPost.sh
@@ -7,5 +7,5 @@ curl -k --location --request POST '(PostURL)' \
 --header 'Content-Type: application/xml' \
 --data-raw '<?xml version="1.0" encoding="UTF-8"?>
   (rest of API content here)
-    <message type="text/plain">'"$file3"'</message>
+    <message type="text/plain">'"$file1"'</message>
 </ticket>'
